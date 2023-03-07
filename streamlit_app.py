a3578d860017aaaff148653c6e9e3710de443cd7
UMAMAHESWARI-6 <66478401+UMAMAHESWARI-6@users.noreply.github.com>
2023-03-07 23:02:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,7 +44,7 @@ try:
       back_from_function = get_fruityvice_data(fruit_choice)
       streamlit.dataframe(back_from_function)
  #except URLError as e:
- streamlit.error()
+ #streamlit.error()
       
 #streamlit.write('The user entered ', fruit_choice)
 #fruits_selected = streamlit.multiselect("What fruit would you like information about?", list(fruit_choice.index),['Kiwi'])
